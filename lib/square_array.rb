242d5d37d5ec429f73e71b836bff9261bdb1f1d4
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-15 19:23:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,16 +2,16 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-#<<<<<<< HEAD
+<<<<<<< HEAD
     new_array << array[counter]**2
     counter += 1
    end
    new_array
-#=======
+=======
   puts  array[counter]**2
   new_array << square_array(array)
   counter += 1
   
   end
-#>>>>>>> aaaa2322b62c30d6021293feb370f7cf8c3c5b87
+>>>>>>> aaaa2322b62c30d6021293feb370f7cf8c3c5b87
 end
